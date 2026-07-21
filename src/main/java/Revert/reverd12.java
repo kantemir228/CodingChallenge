cd4bc94e8f4be329a64e0db5b1660be321909e24
Kantemir <kantemirdzumakov@gmail.com>
2026-07-21 21:41:58 +0600

    Revert "containsss bagg"
    This reverts commit a3475ce6933fe511f0aaa89cf7d4d960f16930ac.

--- src/main/java/Revert/reverd12.java
+++ src/main/java/Revert/reverd12.java
@@ -3,6 +3,5 @@ package Revert;
 public class reverd12 {
     public static void main(String[] args) {
         System.out.println("Containsbag");
-        System.out.println("baghas beemfixwt");
     }
 }
