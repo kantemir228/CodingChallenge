d98d9de94947bb4244d3ab0ee4fa49884beb6995
Kantemir <kantemirdzumakov@gmail.com>
2026-07-21 10:53:02 +0600

    this commit contains bag

--- src/main/java/Revert/ReverDemo.java
+++ src/main/java/Revert/ReverDemo.java
@@ -3,5 +3,6 @@ package Revert;
 public class ReverDemo {
     public static void main(String[] args) {
         System.out.println("Thhis line hhas a bag!!!");
+        System.out.println("This bug has been fixed!!!");
     }
 }
