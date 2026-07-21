a3475ce6933fe511f0aaa89cf7d4d960f16930ac
Kantemir <kantemirdzumakov@gmail.com>
2026-07-21 21:40:36 +0600

    containsss bagg

--- src/main/java/Revert/reverd12.java
+++ src/main/java/Revert/reverd12.java
@@ -3,5 +3,6 @@ package Revert;
 public class reverd12 {
     public static void main(String[] args) {
         System.out.println("Containsbag");
+        System.out.println("baghas beemfixwt");
     }
 }
