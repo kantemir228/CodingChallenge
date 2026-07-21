5db277577804716e73f553014bf81dfe24f7f4aa
Kantemir <kantemirdzumakov@gmail.com>
2026-07-21 21:29:21 +0600

    qwe

--- src/main/java/Revert/reverd.java
+++ src/main/java/Revert/reverd.java
@@ -3,5 +3,7 @@ package Revert;
 public class reverd {
     public static void main(String[] args) {
         System.out.println("Contains bag");
+        System.out.println("Contains yui ry bag");
+
     }
 }
