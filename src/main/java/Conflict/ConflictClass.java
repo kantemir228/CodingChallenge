0f21711fcc32667da0c9baaf8c3b2ed892f33075
Kantemir <kantemirdzumakov@gmail.com>
2026-07-22 09:41:42 +0600

    sunny day

--- src/main/java/Conflict/ConflictClass.java
+++ src/main/java/Conflict/ConflictClass.java
@@ -2,6 +2,6 @@ package Conflict;
 
 public class ConflictClass {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello worl435d!");
     }
 }
