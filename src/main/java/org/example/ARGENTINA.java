a0e893cf666449c20939a64d87756256870a47e1
Kantemir <kantemirdzumakov@gmail.com>
2026-07-22 10:00:30 +0600

    Argentina bay bay

--- src/main/java/org/example/ARGENTINA.java
+++ src/main/java/org/example/ARGENTINA.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class ARGENTINA {
     public static void main(String[] args) {
-        System.out.println("Hello argentina");
+        System.out.println(" bay bay argentina");
     }
 }
